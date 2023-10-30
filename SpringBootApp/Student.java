5dd0a39383dace3771a4e21ae184b734417e9669
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-10-31 00:13:29 +0530

    Adding Student Entity

--- SpringBootApp/Student.java
+++ SpringBootApp/Student.java
@@ -1,1 +1,43 @@
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel(description = "Student Entity")
+public class Student {
+    @ApiModelProperty(notes = "The student's ID")
+    private Long id;
+    @ApiModelProperty(notes = "The student's name")
+    private String name;
+    @ApiModelProperty(notes = "The student's roll number")
+    private String rollNumber;
+
+    public Student() {}
+
+    public Student(String name, String rollNumber) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+}
