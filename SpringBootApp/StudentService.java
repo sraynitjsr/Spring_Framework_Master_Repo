6e46723655cb115f47cf49bbdfd9207c1256b3c5
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-10-31 00:14:42 +0530

    Adding StudentService Interface

--- SpringBootApp/StudentService.java
+++ SpringBootApp/StudentService.java
@@ -1,1 +1,8 @@
+import java.util.List;
 
+public interface StudentService {
+    Student getStudentByName(String name);
+    Student getStudentByRollNumber(String rollNumber);
+    Student addStudent(Student student);
+    List<Student> getAllStudents();
+}
