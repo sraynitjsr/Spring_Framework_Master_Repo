d1f1cc183541471759e42ced89bc49ea2d3bf16d
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-10-31 00:14:13 +0530

    Adding StudentRepository

--- SpringBootApp/StudentRepository.java
+++ SpringBootApp/StudentRepository.java
@@ -1,1 +1,6 @@
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Student findByName(String name);
+    Student findByRollNumber(String rollNumber);
+}
