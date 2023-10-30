49cf36c4a5cc29bb93c32624423e0c22cddd6905
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-10-31 00:16:46 +0530

    Adding Controllers For Student Service

--- SpringBootApp/StudentController.java
+++ SpringBootApp/StudentController.java
@@ -1,1 +1,37 @@
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@RequestMapping("/students")
+@Api(value = "Student Management System", description = "Operations pertaining to student management")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/byName/{name}")
+    @ApiOperation(value = "Get a student by name", response = Student.class)
+    public Student getStudentByName(@PathVariable String name) {
+        return studentService.getStudentByName(name);
+    }
+
+    @GetMapping("/byRollNumber/{rollNumber}")
+    @ApiOperation(value = "Get a student by roll number", response = Student.class)
+    public Student getStudentByRollNumber(@PathVariable String rollNumber) {
+        return studentService.getStudentByRollNumber(rollNumber);
+    }
+
+    @PostMapping("/add")
+    @ApiOperation(value = "Add a new student", response = Student.class)
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @GetMapping("/all")
+    @ApiOperation(value = "Get all students", response = Student.class)
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+}
