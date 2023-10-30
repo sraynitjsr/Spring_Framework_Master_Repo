25979a527f30936b5a9fb2d3da424172d88ba388
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-10-31 00:15:46 +0530

    Adding Student Service Implementation For Student Service

--- SpringBootApp/StudentServiceImpl.java
+++ SpringBootApp/StudentServiceImpl.java
@@ -1,1 +1,30 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student getStudentByName(String name) {
+        return studentRepository.findByName(name);
+    }
+
+    @Override
+    public Student getStudentByRollNumber(String rollNumber) {
+        return studentRepository.findByRollNumber(rollNumber);
+    }
+
+    @Override
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
