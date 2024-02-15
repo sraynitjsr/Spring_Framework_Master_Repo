1f44878660854859333bb8cd055054c9ef528de5
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-02-15 23:40:42 +0530

    Controller Layer For User REST API

---
+++ Spring_Boot_JPA/UserController.java
@@ -0,0 +1,41 @@
+package com.example.springbootmongodbexample.controller;
+
+import com.example.springbootmongodbexample.model.User;
+import com.example.springbootmongodbexample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+    }
+}
