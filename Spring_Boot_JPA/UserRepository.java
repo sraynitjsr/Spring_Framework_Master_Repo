30a46ed84c97147dfd31fde5a37071205aad5d9e
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-02-16 00:18:01 +0530

    Finding Users By Email ID Domain Using @Query DSL

--- Spring_Boot_JPA/UserRepository.java
+++ Spring_Boot_JPA/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.springbootmongodbexample.repository;
 
 import com.example.springbootmongodbexample.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     List<User> findByNameStartingWith(String prefix); // Custom query method
+
+    @Query("{'email' : {$regex : ?0}}")
+    List<User> findByEmailRegex(String domain); // Custom query method using @Query annotation
 }
