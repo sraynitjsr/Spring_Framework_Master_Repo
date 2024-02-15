3c68614fd82c66422ed2701b6da7cbf8949affe9
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-02-16 00:09:25 +0530

    Finding Users That Match Given String Prefix

--- Spring_Boot_JPA/UserRepository.java
+++ Spring_Boot_JPA/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.springbootmongodbexample.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+    List<User> findByNameStartingWith(String prefix); // Custom query method
 }
