000bc7d7fabfce7a9001d5e5fbedae407ec6d1a6
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-02-15 23:42:11 +0530

    DAO Layer

---
+++ Spring_Boot_JPA/UserService.java
@@ -0,0 +1,13 @@
+package com.example.springbootmongodbexample.service;
+
+import com.example.springbootmongodbexample.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(String id);
+    User addUser(User user);
+    User updateUser(String id, User user);
+    void deleteUser(String id);
+}
