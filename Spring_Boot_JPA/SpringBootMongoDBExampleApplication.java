9665910d22cb29a6add42af83f812db55f54bdfb
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-02-15 23:39:21 +0530

    Main Class REST API JPA

---
+++ Spring_Boot_JPA/SpringBootMongoDBExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootmongodbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoDBExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongoDBExampleApplication.class, args);
+    }
+}
