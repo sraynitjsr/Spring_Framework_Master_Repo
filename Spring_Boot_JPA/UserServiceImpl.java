627a8e29d6ba6bbd24c30bcbd5930816840112a7
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-02-15 23:41:36 +0530

    Service Layer Implementation For User REST APIs

---
+++ Spring_Boot_JPA/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.springbootmongodbexample.service;
+
+import com.example.springbootmongodbexample.model.User;
+import com.example.springbootmongodbexample.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(String id, User user) {
+        if (userRepository.existsById(id)) {
+            user.setId(id);
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+}
